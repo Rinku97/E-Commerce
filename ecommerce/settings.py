fb653b1c100e3433780e1313ac40742b27204743
Rinku97 <rinkurb0@gmail.com>
2020-12-21 15:25:34 +0530

    changed Debug Setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm98fi-3ex0ppj0*o=7uf-p_2hb8+(t204o7&qokjvt(ib2fkfp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
